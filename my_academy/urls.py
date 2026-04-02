b5ebaf53e32fca4338cb6dccc5e78f78caa240b0
Otabek <otabekkarimov1421@gmail.com>
2026-04-03 02:19:35 +0500

    URL patterns xatosi tuzatildi

--- my_academy/urls.py
+++ my_academy/urls.py
@@ -1,7 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+# Admin yaratish funksiyasi
 def create_my_admin(request):
     user, created = User.objects.get_or_create(username='admin')
-    user.set_password('123') # <--- PAROLNI 123 QILDIK!
+    user.set_password('123')
     user.is_superuser = True
     user.is_staff = True
     user.save()
     return HttpResponse("Tayyor! Login: admin, Parol: 123. Endi bemalol kiring!")
+
+# URL naqshlari - SHU JOYI ETIBORLI BO'LSIN!
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('makeadmin/', create_my_admin),
+    path('', include('akademiyam.urls')), # Sening asosiy sahifang
+]
