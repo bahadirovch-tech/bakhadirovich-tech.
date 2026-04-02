edeb4cb74455d731a7aaf5238edb168d615127f2
Otabek <otabekkarimov1421@gmail.com>
2026-04-03 01:09:31 +0500

    Settings to'liq tuzatildi

--- my_academy/settings.py
+++ my_academy/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-otabek-tuit-academy'
 DEBUG = True
-ALLOWED_HOSTS = ['bakhadirovich-tech.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['bakhadirovich-tech.onrender.com', '127.0.0.1', 'localhost', '*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,5 +58,10 @@ LANGUAGE_CODE = 'uz-uz'
 TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True
+
+# Statik fayllar (CSS, rasmlar) sozlamalari
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
