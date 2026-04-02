d0d0499b291faf9e15a3f66eac2b363d2f8a2262
Otabek <otabekkarimov1421@gmail.com>
2026-04-03 01:48:25 +0500

    Admin funksiyasi 100 foiz qoshildi

--- my_academy/urls.py
+++ my_academy/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # <--- include ham bo'lsin
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
-# Maxfiy "shpion" funksiya - bir marta admin yaratish uchun
 def create_my_admin(request):
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser('admin', 'admin@mail.com', 'admin123')
@@ -12,5 +11,6 @@ def create_my_admin(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('makeadmin/', create_my_admin), # <--- MANA SHU YERGA KIRASIZ
+    path('makeadmin/', create_my_admin), # <--- MANA SHU YO'L KERAK
+    path('', include('akademiyam.urls')), # Sening asosiy sahifang
 ]
