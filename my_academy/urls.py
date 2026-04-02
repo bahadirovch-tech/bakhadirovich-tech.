a7e92dc59dcbf147ba94e3527cc251a2f0756d54
Otabek <otabekkarimov1421@gmail.com>
2026-04-03 01:57:38 +0500

    Admin paroli 123 qilindi

--- my_academy/urls.py
+++ my_academy/urls.py
@@ -1,16 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include # <--- include ham bo'lsin
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-
 def create_my_admin(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@mail.com', 'admin123')
-        return HttpResponse("Tabriklaymiz! Admin yaratildi. Login: admin, Parol: admin123")
-    return HttpResponse("Admin shundoq ham bor!")
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('makeadmin/', create_my_admin), # <--- MANA SHU YO'L KERAK
-    path('', include('akademiyam.urls')), # Sening asosiy sahifang
-]
+    user, created = User.objects.get_or_create(username='admin')
+    user.set_password('123') # <--- PAROLNI 123 QILDIK!
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+    return HttpResponse("Tayyor! Login: admin, Parol: 123. Endi bemalol kiring!")
